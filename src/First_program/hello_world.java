158c9ed1f8249458a12d965d2ac3a80e04667a5e
沈先生 <51504384+13177833987@users.noreply.github.com>
2020-01-15 21:45:08 +0800

    1.1_test

--- src/First_program/hello_world.java
+++ src/First_program/hello_world.java
@@ -3,6 +3,7 @@ package First_program;
 public class hello_world {
 
 	public static void main(String[] args) {
+
 		System.out.println("Hello,world! ÐÞ¸Ä1.0°æ±¾");
 	}
 
---
+++ src/input_scanner/testInput.java
@@ -0,0 +1,15 @@
+package input_scanner;
+
+import java.util.Scanner;
+
+public class testInput {
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		int y=s.nextInt();
+		s.close();
+		System.out.println(y);
+
+	}
+
+}
