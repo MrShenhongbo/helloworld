6331ab84426eac7a0ad33819011fe49f805189ae
Mrshen <1220974104@qq.com>
2020-10-21 00:02:52 +0800

    commit提交冲突文件到本地库，然后解决冲突

--- src/First_program/hello_world.java
+++ src/First_program/hello_world.java
@@ -6,6 +6,10 @@ public class hello_world {
 
 		//TODO 2020-10-20 23:40:44 Git cloned第一次修改
 		System.out.println("Hello,World!");
+		
+		if (true) {
+			System.out.println("第二次修改，测试冲突");
+		}
 	}
 
 }
