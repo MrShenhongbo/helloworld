bfa465b4f2f579a77feca3ffcad2684c9074ddc5
敖丙 <shen_hongbo@polarwind.com.cn>
2020-10-22 09:17:29 +0800

    提交测试

--- src/ecut/Person.java
+++ src/ecut/Person.java
@@ -5,6 +5,8 @@ public class Person {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     System.out.println("I am a Person");
+    
+    //TODO 2020-10-22 09:16:47 再次修改后提交代码
   }
 
 }
